747f769020314237e0a2bdfb8dd5d8871f6931f6
puzevich <s.rudaya@sam-solutions.com>
2025-10-22 14:16:38 +0300

    feat: implement worker threads

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,28 @@
+import os from 'os';
+import { Worker } from 'worker_threads';
+
 const performCalculations = async () => {
-  // Write your code here
+  const cpus = os.cpus().length;
+  console.log(`Processing ${cpus} cpus`);
+  let fib = 10;
+  const workers = [];
+  for (let i = 0; i < cpus; i++) {
+    workers.push(new Promise((resolve, reject) => {
+      const worker = new Worker('./worker.js', { workerData: fib });
+      worker.on('message', (data) => {
+        resolve({ status: 'resolved', data })
+      });
+      worker.on('error', () => resolve({ status: 'error', data: null }));
+      worker.on('exit', (code) => {
+        if (code !== 0)
+          reject(new Error(`Worker stopped with exit code ${code}`));
+      });
+    }));
+    fib++;
+  }
+  Promise.all(workers).then((result) => {
+    console.log(result);
+  })
 };
 
 await performCalculations();
