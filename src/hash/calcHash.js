8785f2f9fd68777a73eee67128d889e05277607f
puzevich <s.rudaya@sam-solutions.com>
2025-10-22 14:14:53 +0300

    feat: implement hash function

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,24 @@
+import fs from 'fs';
+import crypto from 'crypto';
+
 const calculateHash = async () => {
-  // Write your code here
+  const file = './files/fileToCalculateHashFor.txt';
+  const digest = await new Promise((resolve, reject) => {
+    const hash = crypto.createHash('sha256');
+    const stream = fs.createReadStream(file);
+
+    stream.on('data', (chunk) => {
+      hash.update(chunk);
+    });
+
+    stream.on('end', () => {
+      resolve(hash.digest('hex'));
+    });
+
+    stream.on('error', reject);
+
+  });
+  console.log(digest);
 };
 
 await calculateHash();
